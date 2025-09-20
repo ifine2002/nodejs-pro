6ba3aff08d87a9926aef65d86af9dccb3ca2acb4
ifine2002 <boyyhp1@gmail.com>
2025-09-20 16:14:21 +0700

    set up .env

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
+import 'dotenv/config';
+
 const express = require('express');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Hello World Nodemon');
 })
 
 app.listen(8080, () => {
     console.log(`Server is running on port: ${PORT}`);
+    console.log("env port:", process.env.PORT)
 })
