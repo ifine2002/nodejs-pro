0d1b2dd01f06f050e05735d70c47d5cb7e9422af
ifine2002 <boyyhp1@gmail.com>
2025-09-21 01:21:28 +0700

    fetch all user, create a new user

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,6 @@ app.use(express.static('public'));
 //config routes
 webRoutes(app);
 
-getConnection();
-
 app.listen(8080, () => {
     console.log(`Server is running on port: ${PORT}`);
 })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,37 @@
+import getConnection from "../config/database";
 
-const handleCreateUser = (name: string, email: string, address: string) => {
+const handleCreateUser = async (name: string, email: string, address: string) => {
     //logic to create user
+    const connection = await getConnection();
+    // execute will internally call prepare and query
+    try {
+        const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+        const values = [name, email, address];
 
+        const [result, fields] = await connection.execute(sql, values);
+        return result;
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
     //return some result
     console.log('>>> insert a new user');
 }
 
-export { handleCreateUser };
+const getAllUsers = async () => {
+    //logic to get all users
+
+    const connection = await getConnection();
+    // A simple SELECT query
+    try {
+        const [results, fields] = await connection.query(
+            'SELECT * FROM `users`'
+        );
+        return results;
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+
+export { handleCreateUser, getAllUsers };
